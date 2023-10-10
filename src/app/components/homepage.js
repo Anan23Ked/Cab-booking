92d31ff2ba640b223d2e03e6f5bd93376bfbe305
Anan23Ked <87655973+Anan23Ked@users.noreply.github.com>
2023-10-10 17:22:56 +0530

    Add files via upload

---
+++ src/app/components/homepage.js
@@ -0,0 +1,7 @@
+const homepage = () =>{
+    return (
+        <div><h2>home page</h2></div>
+    )
+}
+
+export default homepage
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import homepage from './components/homepage'
+
+export default function Home() {
+  return (
+    <div>
+      <homepage />
+
+      <h1>Hello landing page</h1>
+      
+    
+    </div>
+    
+  )
+}
